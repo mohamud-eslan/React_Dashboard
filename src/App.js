8207fd9063bcf144251c086f89a44ccbf8d96e96
mohamud <mohmud141@gmail.com>
2023-01-29 11:29:13 +0300

    file and folder structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/Fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="underline text-3xl">App test</h1>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+          <TooltipComponent content="settings" position="top">
+            <button>
+              <FiSettings />
+            </button>
+
+          </TooltipComponent>
+
+        </div>
+
+        </div>
+
+      
+      </BrowserRouter>
+     
     </div>
   );
 }
